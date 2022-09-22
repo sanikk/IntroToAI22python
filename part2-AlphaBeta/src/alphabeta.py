951d9540c413b06cc48c7a7df770c8ee5a13beaf
sanikk <sanikk@users.noreply.github.com>
2022-09-23 02:17:14 +0300

    alphabeta

--- part2-AlphaBeta/src/alphabeta.py
+++ part2-AlphaBeta/src/alphabeta.py
@@ -22,7 +22,7 @@ class AlphaBetaNode(object):
 class TicTacToe(AlphaBetaNode):
     """Class that contains current state of the game and implements AlphaBetaNode methods
     :attr state: Current state of the board (str)
-    :attr state: Indicates whose turn it is (Boolean)
+    :attr crosses_turn: Indicates whose turn it is (Boolean)
     """
 
     def __init__(self, state, crosses_turn):
@@ -35,7 +35,8 @@ class TicTacToe(AlphaBetaNode):
 
     def won(self, c):
         triples = [self.state[0:3], self.state[3:6], self.state[6:9], self.state[::3], self.state[1::3],
-                   self.state[2::3], self.state[0] + self.state[4] + self.state[8],
+                   self.state[2::3], self.state[0] +
+                   self.state[4] + self.state[8],
                    self.state[2] + self.state[4] + self.state[6]]
         combo = 3 * c
         return combo in triples
@@ -56,16 +57,24 @@ class TicTacToe(AlphaBetaNode):
         :return: list of TicTacToe objects
         """
         # Implement me
+        palautettava = []
+        for i in range(len(self.state)):
+            if self.state[i] == '?':
+                palautettava.append(
+                    TicTacToe(
+                        f"{self.state[0:i]}{'x' if self.crosses_turn else 'o'}{self.state[i+1:]}", not self.is_max_node()))
+        return palautettava
 
-        return []
-
-    def value(self):
+    def value(self):  # DONE!!
         """
         Current score of the game (0, 1, -1)
         :return: int
         """
         # Implement me
-
+        if self.won('x'):
+            return 1
+        if self.won('o'):
+            return -1
         return 0
 
 
@@ -77,14 +86,33 @@ def alpha_beta_value(node):
 
     # Implement me
 
+    if node.is_max_node():
+        return max_value(node, -1, 1)
+    else:
+        return min_value(node, -1, 1)
+
 
 def max_value(node, alpha, beta):
     # Implement me
-
-    return HUGE_NUMBER
+    if node.is_end_state():
+        return node.value()
+    v = -HUGE_NUMBER
+    for child in node.generate_children():
+        v = max(v, min_value(child, alpha, beta))
+        alpha = max(alpha, v)
+        if alpha >= beta:
+            return v
+    return v
 
 
 def min_value(node, alpha, beta):
     # Implement me
-
-    return +HUGE_NUMBER
+    if node.is_end_state():
+        return node.value()
+    v = HUGE_NUMBER
+    for child in node.generate_children():
+        v = min(v, max_value(child, alpha, beta))
+        beta = min(beta, v)
+        if alpha >= beta:
+            return v
+    return v
--- part2-AlphaBeta/src/main.py
+++ part2-AlphaBeta/src/main.py
@@ -1,17 +1,55 @@
-from alphabeta import TicTacToe
-from alphabeta import alpha_beta_value
+from alphabeta import alpha_beta_value, TicTacToe
+#from alphabeta import alpha_beta_value, TicTacToe
 
 
 def play(state):
     """Makes turn and prints the result of it until the game is over
     :param state: The initial state of the game (TicTacToe)
     """
+
     # Implement me
+    print(state)
+    while not state.is_end_state():
+        best_move = 'I SURRENDER'
+        optimal_found = False
+        if state.is_max_node():
+            v = -10
+            optimal = 1
+        else:
+            v = 10
+            optimal = -1
+        for child in state.generate_children():
+            if optimal_found:
+                continue
+            #print("Handling child")
+            # print(child)
+
+            palautus = alpha_beta_value(child)
+            if palautus == optimal:
+                #print("found optimal!!")
+                optimal_found = True
+                best_move = child
+            elif (optimal == 1 and palautus > v) or (optimal == -1 and palautus < v):
+                best_move = child
+                v = palautus
+
+            # print(f"{palautus =}")
+
+        state = best_move
+        print(state)
+
+
+def testeri(board, whose_turn):
+    print(20 * "-")
+    state = TicTacToe(board, whose_turn)
+    print(f"beginning state:\n{state}")
+    alpha_beta_value(state)
+
 
 def main():
     """You need to implement the following functions/methods:
     play(state): makes turn and prints the result of it until the game is over
-    value() in TicTacToe class: returns the current score of the game
+    X value() in TicTacToe class: returns the current score of the game
     generate_children() in TicTacToe class: returns a list of all possible states after this turn
     alpha_beta_value(node): implements the MinMax algorithm with alpha-beta pruning
     max_value(node, alpha, beta): implements the MinMax algorithm with alpha-beta pruning
@@ -19,8 +57,14 @@ def main():
     """
     empty_board = 3 * '???'
     state = TicTacToe(empty_board, True)
-    print(state)
-    play(state)
+    # play(state)
+    state2 = TicTacToe('o?ox??xo?', True)
+    # play(state2)
+
+    exa_board_2 = 'oox?x?ox?'
+    play(TicTacToe(exa_board_2, True))
+    #print(alpha_beta_value(TicTacToe(exa_board_2, True)))
+    # alpha_beta_value(exa_state_2)
 
 
 if __name__ == '__main__':
